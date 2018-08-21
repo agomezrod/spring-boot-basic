7e15368bef527892b37aadd8774967dfbd3692dc
agomezrod <antonio.gomez.rod@gmail.com>
2018-08-21 11:39:41 +0200

    Initial release

---
+++ src/main/java/com/agr/example/demo/SpringBootBasewebappApplication.java
@@ -0,0 +1,12 @@
+package com.agr.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasewebappApplication.class, args);
+	}
+}
